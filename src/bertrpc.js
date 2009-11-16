54d912f72ed0f4052080043d6d469dfc5dfbf0db
Ryan Tomayko <rtomayko@gmail.com>
2009-11-15 20:20:32 -0800

    less complex server creation

--- src/bertrpc.js
+++ src/bertrpc.js
@@ -31,58 +31,59 @@ var BERTRPC = {
   },
 
   /* the node server */
-  server: function () {
+  server: tcp.createServer(function (socket) {
     var trace = BERTRPC.trace;
-    return tcp.createServer(function (socket) {
-      socket.setEncoding("binary");
-      socket.addListener("connect", function () { trace("-->", "connect"); });
+    socket.setEncoding("binary");
 
-      var buf  = "",
-          size = null;
-
-      socket.addListener("receive", function (data) {
-        buf += data;
-
-        while ( size || buf.length >= 4 ) {
-          if ( size == null ) {
-            size = bytes_to_int(buf, 4);
-            buf = buf.substring(4);
-          } else if ( buf.length >= size ) {
-            var raw = buf.substring(0, size);
-            var term = bert.decode(raw);
-            trace("-->", "" + size + ": " + bert.repr(term));
-
-            // dispatch call to module handler
-            var type = term[0],
-                 mod = term[1],
-                 fun = term[2],
-                args = term[3];
-            var res = BERTRPC.dispatch(type, mod, fun, args);
-
-            // encode and throw back over the wire
-            var reply = t(REPLY, res);
-            socket.send(raw = berp.pack(reply));
-            trace("<--", "" + (raw.length - 4) + ": " + bert.repr(reply));
-
-            // keep eating into the buffer
-            buf = buf.substring(size);
-            size = null;
-          } else {
-            break;
-          }
-        }
-      });
-      socket.addListener("eof", function () {
-        trace("-->", "eof");
-        socket.close();
-        trace("<--", "close");
-      });
+    socket.addListener("connect", function () {
+       trace("-->", "connect");
     });
-  },
+
+    socket.addListener("eof", function () {
+      trace("-->", "eof");
+
+      socket.close();
+      trace("<--", "close");
+    });
+
+    var buf  = "",
+        size = null;
+    socket.addListener("receive", function (data) {
+      buf += data;
+      while ( size || buf.length >= 4 ) {
+        if ( size == null ) {
+          size = bytes_to_int(buf, 4);
+          buf = buf.substring(4);
+        } else if ( buf.length >= size ) {
+          var raw = buf.substring(0, size);
+          var term = bert.decode(raw);
+          trace("-->", "" + size + ": " + bert.repr(term));
+
+          // dispatch call to module handler
+          var type = term[0],
+               mod = term[1],
+               fun = term[2],
+              args = term[3];
+          var res = BERTRPC.dispatch(type, mod, fun, args);
+
+          // encode and throw back over the wire
+          var reply = t(REPLY, res);
+          socket.send(raw = berp.pack(reply));
+          trace("<--", "" + (raw.length - 4) + ": " + bert.repr(reply));
+
+          // keep eating into the buffer
+          buf = buf.substring(size);
+          size = null;
+        } else {
+          break;
+        }
+      }
+    });
+  }),
 
   /* begin listing on the port and host specified */
   listen: function (port, host) {
-    BERTRPC.server().listen(port, host);
+    BERTRPC.server.listen(port, host);
   }
 };
 
