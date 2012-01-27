c78a32fd52be5c52bb08a04ddb81c1962b5bde0c
Jeffrey Hulten <jeffh@automatedlabs.com>
2012-01-26 16:47:52 -0800

    Converted complex dictionary tests to nodeunit and added to test_bert_dict.js

--- test/bert-test.js
+++ test/bert-test.js
@@ -15,34 +15,3 @@ var data = null,
     obj  = null;
 
 
-
-// COMPLEX DICTIONARY
-
-obj = { a: bert.tuple(1, 2, 3), b: [4, 5, 6] }
-
-test('bert.repr(<complex>)', function() {
-   dump(util.inspect(obj));
-   dump(R(obj));
-   assert_equal("[{a, {1, 2, 3}}, {b, [4, 5, 6]}]", bert.repr(obj));
-});
-
-test('bert.encode(<complex>)', function () {
-   data = encode(obj);
-   assert_equal(
-      "<<131,104,3,100,0,4,98,101,114,116,100,0,4,100,105,99,116,108,"  +
-      "0,0,0,2,104,2,100,0,1,97,104,3,97,1,97,2,97,3,104,2,100,0,1,98," +
-      "108,0,0,0,3,97,4,97,5,97,6,106,106>>",
-      bin(data)
-   );
-});
-
-test('bert.decode(<complex>)', function() {
-   obj = decode(data);
-   dump(util.inspect(obj));
-   dump(R(obj));
-   assert_equal('object', typeof(obj));
-   assert_equal('tuple',  obj.a.type);
-   assert_equal(1, obj.a[0]);
-   assert_equal(2, obj.a[1]);
-   assert_equal(3, obj.a[2]);
-});
--- test/test_bert_dictionary.js
+++ test/test_bert_dictionary.js
@@ -21,3 +21,30 @@ exports.testDictDecode = function(test) {
     test.strictEqual(obj['c'], 3);
     test.done();
 };
+
+exports.testComplexDictRepr = function(test) {
+    var obj = { a: bert.tuple(1, 2, 3), b: [4, 5, 6] };
+    test.strictEqual(bert.repr(obj), "[{a, {1, 2, 3}}, {b, [4, 5, 6]}]");
+    test.done();
+};
+
+exports.testComplexDictEncode = function(test) {
+    var obj = { a: bert.tuple(1, 2, 3), b: [4, 5, 6] };
+    var data = bert.encode(obj);
+    var expected = "<<131,104,3,100,0,4,98,101,114,116,100,0,4,100,105,99,116,108,"  +
+        "0,0,0,2,104,2,100,0,1,97,104,3,97,1,97,2,97,3,104,2,100,0,1,98," +
+        "108,0,0,0,3,97,4,97,5,97,6,106,106>>";
+    test.strictEqual(bert.bin_repr(data), expected);
+    test.done();
+};
+
+exports.testComplexDictDecode = function(test) {
+    var data = bert.encode({ a: bert.tuple(1, 2, 3), b: [4, 5, 6] });
+    var obj = bert.decode(data);
+    test.strictEqual(typeof(obj), 'object');
+    test.strictEqual(obj.a.type, 'tuple');
+    test.strictEqual(obj.a[0], 1);
+    test.strictEqual(obj.a[1], 2);
+    test.strictEqual(obj.a[2], 3);
+    test.done();
+};
